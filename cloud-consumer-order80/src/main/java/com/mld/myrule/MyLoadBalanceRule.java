5ea0795ec1208a28c25985087c15119d6fafd19e
daibiao <db_work@126.com>
2021-02-24 23:08:03 +0800

    ribbon 负载均衡 备注

--- cloud-consumer-order80/src/main/java/com/mld/myrule/MyLoadBalanceRule.java
+++ cloud-consumer-order80/src/main/java/com/mld/myrule/MyLoadBalanceRule.java
@@ -5,6 +5,10 @@ import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+/**
+ * ribbon 默认的负载均衡策略是轮询，原理是 rest接口第几次请求次数%服务集群总数=实际调用的服务器位置下标，
+ * 每次重启服务或达到Integer.MAX时rest接口计数从1开始重新计数。
+ */
 @Component
 public class MyLoadBalanceRule {
 
